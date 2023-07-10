89b3785712b8b33bffdbb5142f298d03a07a6891
mfgmgrAbbas <smasnb12@gmail.com>
2023-07-10 20:51:40 +0500

    update

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,25 +1,30 @@
-// import getBillboard from "@/actions/get-billboard";
-// import getProducts from "@/actions/get-products";
-// import ProductList from "@/components/product-list";
-// import Billboard from "@/components/ui/billboard";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
+
 
 export const revalidate = 0;
 
 const HomePage = async () => {
-  // const products = await getProducts({ isFeatured: true });
-  // const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
 
+ const products = await getProducts({isFeatured: true})
+  const billboard = await getBillboard('30091632-d150-484e-9335-2193fc9c5f5a');
+                                          
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        {/* <Billboard 
+        <Billboard 
           data={billboard}
         />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
-        </div> */}
-      </div>
+        </div>
+
+       </div>
+     
     </Container>
   )
 };
