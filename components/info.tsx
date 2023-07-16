dbac2fa65fb71733cb517d09130fd4f160faefb5
mfgmgrAbbas <smasnb12@gmail.com>
2023-07-16 15:13:30 +0500

    update

--- components/info.tsx
+++ components/info.tsx
@@ -1,52 +1,96 @@
-"use client";
+'use client'
 
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, Plus, Minus } from "lucide-react";
 
-import Currency  from "@/components/ui/currency";
+import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import { Product } from "@/types";
 import useCart from "@/hooks/use-cart";
+import { useState } from "react";
 
 interface InfoProps {
-  data: Product
-};
+  data: Product;
+}
 
 const Info: React.FC<InfoProps> = ({ data }) => {
   const cart = useCart();
+  const [quantity, setQuantity] = useState(1);
 
   const onAddToCart = () => {
-    cart.addItem(data);
-  }
+    for (let i = 0; i < quantity; i++) {
+      cart.addItem(data);
+    }
+  };
 
-  return ( 
+  const increaseQuantity = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity((prevQuantity) => prevQuantity - 1);
+    }
+  };
+
+  const price = parseFloat(data?.price);
+  const totalPrice = price * quantity;
+
+  return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
       <div className="mt-3 flex items-end justify-between">
         <p className="text-2xl text-gray-900">
-          <Currency value={data?.price} />
+          <Currency value={price} />
         </p>
       </div>
       <hr className="my-4" />
       <div className="flex flex-col gap-y-6">
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Size:</h3>
-          <div>
-            {data?.size?.value}
-          </div>
+          <div>{data?.size?.value}</div>
         </div>
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Color:</h3>
-          <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.color?.value }} />
+          <div
+            className="h-6 w-6 rounded-full border border-gray-600"
+            style={{ backgroundColor: data?.color?.value }}
+          />
         </div>
-      </div>
-      <div className="mt-10 flex items-center gap-x-3">
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-black">Quantity:</h3>
+          <Button
+          onClick={decreaseQuantity}
+          className="flex items-center gap-x-2"
+        >
+          <Minus size={16} />
+        </Button>
+        <input
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(parseInt(e.target.value))}
+          className="w-12 text-center"
+        />
+        <Button onClick={increaseQuantity} className="flex items-center gap-x-2">
+          <Plus size={16} />
+        </Button>
+        </div>
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-black">Total Price:</h3>
+          <Currency value={totalPrice} />
+        </div>
+
+        
+       <div className="mt-10 flex items-center gap-x-3 ">
         <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add To Cart
           <ShoppingCart size={20} />
         </Button>
+        </div>
+        
       </div>
+      
     </div>
   );
-}
- 
+};
+
 export default Info;
